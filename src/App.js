c8224ce62062ace534e1cd0acd63021008c8709f
Adarsh <adasi@softwareag.com>
2024-07-17 17:34:08 +0530

    2nd commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="rectangle green"></div>
+      <div className="rectangle red"></div>
+      <div className="rectangle blue"></div>
+      <div className="rectangle yellow"></div>
+      <div className="text-container">
+        <div className="text1">
+          <strong>HTML<span>&</span>CSS</strong>
+        </div>
+        <div className="text2">
+          design and build Webcases
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
